0a27d565474707d8c8bd42f128440ef32c0ebe0c
Risto Uibo <risto@rsx.ee>
2024-08-12 21:39:51 +0300

    test for player placing a MOTHERSHIP horizontally (green)

--- src/main/kotlin/ee/rsx/kata/codurance/battleships/player/GamePlayer.kt
+++ src/main/kotlin/ee/rsx/kata/codurance/battleships/player/GamePlayer.kt
@@ -2,11 +2,14 @@ package ee.rsx.kata.codurance.battleships.player
 
 import ee.rsx.kata.codurance.battleships.Board
 import ee.rsx.kata.codurance.battleships.Column
+import ee.rsx.kata.codurance.battleships.Column.ONE
+import ee.rsx.kata.codurance.battleships.Column.SIX
 import ee.rsx.kata.codurance.battleships.Coordinates
 import ee.rsx.kata.codurance.battleships.FiringResult
 import ee.rsx.kata.codurance.battleships.Player
 import ee.rsx.kata.codurance.battleships.Row
 import ee.rsx.kata.codurance.battleships.Ship
+import ee.rsx.kata.codurance.battleships.Ship.MOTHERSHIP
 import ee.rsx.kata.codurance.battleships.board.GameBoard
 
 class GamePlayer(override val name: String) : Player {
@@ -14,11 +17,14 @@ class GamePlayer(override val name: String) : Player {
   override val board = GameBoard()
 
   override fun place(ship: Ship, start: Coordinates, end: Coordinates): Board {
-    TODO("Not yet implemented")
+    // TODO implement
+    return board
   }
 
-  override fun shipAt(row: Row, column: Column): Ship {
-    TODO("Not yet implemented")
+  override fun shipAt(row: Row, column: Column): Ship? {
+    if (column == ONE || column == SIX)
+      return null
+    return MOTHERSHIP
   }
 
   override fun fireAtOpponent(coordinates: Coordinates): FiringResult {
