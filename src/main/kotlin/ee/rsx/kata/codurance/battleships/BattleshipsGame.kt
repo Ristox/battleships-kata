cb3a5a6f2047c40a2d9edd5bc59994706af55e73
Risto Uibo <risto@rsx.ee>
2024-08-12 18:33:24 +0300

    test for game allowing up to 2 players (green)

--- src/main/kotlin/ee/rsx/kata/codurance/battleships/BattleshipsGame.kt
+++ src/main/kotlin/ee/rsx/kata/codurance/battleships/BattleshipsGame.kt
@@ -4,8 +4,13 @@ import ee.rsx.kata.codurance.battleships.player.GamePlayer
 
 class BattleshipsGame : Battleships {
 
+  private val players = mutableListOf<GamePlayer>()
+
   override fun addPlayer(name: String): Player {
-    return GamePlayer(name)
+    check(players.size < 2) { "Maximum of 2 players can be added" }
+    val player = GamePlayer(name)
+    players.add(player)
+    return player
   }
 
   override fun start() {
