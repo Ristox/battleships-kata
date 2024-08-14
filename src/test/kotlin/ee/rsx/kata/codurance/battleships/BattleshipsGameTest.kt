b81cb49d56776f121a65b690ec241f081a283898
Risto Uibo <risto@rsx.ee>
2024-08-14 22:36:03 +0300

    refactor

--- src/test/kotlin/ee/rsx/kata/codurance/battleships/BattleshipsGameTest.kt
+++ src/test/kotlin/ee/rsx/kata/codurance/battleships/BattleshipsGameTest.kt
@@ -1,6 +1,5 @@
 package ee.rsx.kata.codurance.battleships
 
-import ee.rsx.kata.codurance.battleships.Column.*
 import ee.rsx.kata.codurance.battleships.Row.*
 import ee.rsx.kata.codurance.battleships.ShipType.DESTROYER
 import ee.rsx.kata.codurance.battleships.ShipType.GUNSHIP
@@ -77,7 +76,7 @@ class BattleshipsGameTest {
   @Test
   fun `placing a ship places the MOTHERSHIP on the board horizontally`() {
     with(game.addPlayer("John")) {
-      place(MOTHERSHIP, start = Coordinates(E, 2), end = Coordinates(E, 5))
+      place(MOTHERSHIP, from(E, 2), to(E, 5))
 
       assertThat(shipTypeAt(E, 1)).isNull()
       assertThat(shipTypeAt(E, 2)).isEqualTo(MOTHERSHIP)
@@ -91,7 +90,7 @@ class BattleshipsGameTest {
   @Test
   fun `placing a ship places a DESTROYER on the board horizontally`() {
     with(game.addPlayer("John")) {
-      place(DESTROYER, start = Coordinates(G, 6), end = Coordinates(G, 8))
+      place(DESTROYER, from(G, 6), to(G, 8))
 
       assertThat(shipTypeAt(G, 5)).isNull()
       assertThat(shipTypeAt(G, 6)).isEqualTo(DESTROYER)
@@ -104,7 +103,7 @@ class BattleshipsGameTest {
   @Test
   fun `placing a ship places a DESTROYER on the board vertically`() {
     with(game.addPlayer("John")) {
-      place(DESTROYER, start = Coordinates(C, 4), end = Coordinates(E, 4))
+      place(DESTROYER, from(C, 4), to(E, 4))
 
       assertThat(shipTypeAt(B, 4)).isNull()
       assertThat(shipTypeAt(C, 4)).isEqualTo(DESTROYER)
@@ -117,7 +116,7 @@ class BattleshipsGameTest {
   @Test
   fun `placing a ship places a MOTHERSHIP on the board vertically`() {
     with(game.addPlayer("John")) {
-      place(MOTHERSHIP, start = Coordinates(F, 2), end = Coordinates(I, 2))
+      place(MOTHERSHIP, from(F, 2), to(I, 2))
 
       assertThat(shipTypeAt(E, 2)).isNull()
       assertThat(shipTypeAt(F, 2)).isEqualTo(MOTHERSHIP)
@@ -132,7 +131,7 @@ class BattleshipsGameTest {
   fun `placing a MOTHERSHIP across different rows and columns throws exception`() {
     with(game.addPlayer("John")) {
 
-      val test: () -> Unit = { place(MOTHERSHIP, start = Coordinates(E, 5), end = Coordinates(G, 6)) }
+      val test: () -> Unit = { place(MOTHERSHIP, from(E, 5), to(G, 6)) }
 
       assertThrows<IllegalArgumentException>(test).let {
         assertThat(it.message).isEqualTo("Ship must be placed horizontally or vertically")
@@ -144,7 +143,7 @@ class BattleshipsGameTest {
   fun `placing a DESTROYER horizontally to column span shorter than its size throws exception`() {
     with(game.addPlayer("John")) {
 
-      val test: () -> Unit = { place(DESTROYER, start = Coordinates(E, 5), end = Coordinates(E, 6)) }
+      val test: () -> Unit = { place(DESTROYER, from(E, 5), to(E, 6)) }
 
       assertThrows<IllegalArgumentException>(test).let {
         assertThat(it.message).isEqualTo("Given coordinates length (2) is less than ship size (3)")
@@ -156,7 +155,7 @@ class BattleshipsGameTest {
   fun `placing a DESTROYER vertically to row span shorter than its size throws exception`() {
     with(game.addPlayer("John")) {
 
-      val test: () -> Unit = { place(DESTROYER, start = Coordinates(E, 10), end = Coordinates(F, 10)) }
+      val test: () -> Unit = { place(DESTROYER, from(E, 10), to(F, 10)) }
 
       assertThrows<IllegalArgumentException>(test).let {
         assertThat(it.message).isEqualTo("Given coordinates length (2) is less than ship size (3)")
@@ -168,7 +167,7 @@ class BattleshipsGameTest {
   fun `placing a DESTROYER horizontally to column span longer than its size throws exception`() {
     with(game.addPlayer("John")) {
 
-      val test: () -> Unit = { place(DESTROYER, start = Coordinates(E, 5), end = Coordinates(E, 8)) }
+      val test: () -> Unit = { place(DESTROYER, from(E, 5), to(E, 8)) }
 
       assertThrows<IllegalArgumentException>(test).let {
         assertThat(it.message).isEqualTo("Given coordinates length (4) is larger than ship size (3)")
@@ -180,7 +179,7 @@ class BattleshipsGameTest {
   fun `placing a MOTHERSHIP vertically to row span longer than its size throws exception`() {
     with(game.addPlayer("John")) {
 
-      val test: () -> Unit = { place(MOTHERSHIP, start = Coordinates(B, 1), end = Coordinates(B, 5)) }
+      val test: () -> Unit = { place(MOTHERSHIP, from(B, 1), to(B, 5)) }
 
       assertThrows<IllegalArgumentException>(test).let {
         assertThat(it.message).isEqualTo("Given coordinates length (5) is larger than ship size (4)")
@@ -192,7 +191,7 @@ class BattleshipsGameTest {
   fun `placing a DESTROYER horizontally succeeds also, when start column is larger than end column`() {
     with(game.addPlayer("John")) {
 
-      val test: () -> Unit = { place(DESTROYER, start = Coordinates(E, 10), end = Coordinates(E, 8)) }
+      val test: () -> Unit = { place(DESTROYER, from(E, 10), to(E, 8)) }
 
       assertDoesNotThrow(test)
     }
@@ -202,7 +201,7 @@ class BattleshipsGameTest {
   fun `placing a MOTHERSHIP vertically succeeds also, when start row is larger than end row`() {
     with(game.addPlayer("John")) {
 
-      val test: () -> Unit = { place(MOTHERSHIP, start = Coordinates(G, 3), end = Coordinates(D, 3)) }
+      val test: () -> Unit = { place(MOTHERSHIP, from(G, 3), to(D, 3)) }
 
       assertDoesNotThrow(test)
     }
@@ -211,10 +210,10 @@ class BattleshipsGameTest {
   @Test
   fun `placing a MOTHERSHIP vertically and DESTROYER horizontally fails, when they cross`() {
     with(game.addPlayer("John")) {
-      place(MOTHERSHIP, start = Coordinates(D, 4), end = Coordinates(G, 4))
+      place(MOTHERSHIP, from(D, 4), to(G, 4))
 
       val test: () -> Unit = {
-        place(DESTROYER, start = Coordinates(F, 3), end = Coordinates(F, 5))
+        place(DESTROYER, from(F, 3), to(F, 5))
       }
 
       assertThrows<IllegalStateException>(test).let {
@@ -226,10 +225,10 @@ class BattleshipsGameTest {
   @Test
   fun `placing two DESTROYERS horizontally fails, when they overlap each other`() {
     with(game.addPlayer("John")) {
-      place(DESTROYER, start = Coordinates(H, 2), end = Coordinates(H, 4))
+      place(DESTROYER, from(H, 2), to(H, 4))
 
       val test: () -> Unit = {
-        place(DESTROYER, start = Coordinates(H, 3), end = Coordinates(H, 5))
+        place(DESTROYER, from(H, 3), to(H, 5))
       }
 
       assertThrows<IllegalStateException>(test).let {
@@ -241,10 +240,10 @@ class BattleshipsGameTest {
   @Test
   fun `placing two ships (eg DESTROYERS) fails, when they touch each other (coordinates are adjacent)`() {
     with(game.addPlayer("John")) {
-      place(DESTROYER, start = Coordinates(H, 2), end = Coordinates(H, 4))
+      place(DESTROYER, from(H, 2), to(H, 4))
 
       val test: () -> Unit = {
-        place(DESTROYER, start = Coordinates(I, 4), end = Coordinates(I, 6))
+        place(DESTROYER, from(I, 4), to(I, 6))
       }
 
       assertThrows<IllegalStateException>(test).let {
@@ -259,10 +258,10 @@ class BattleshipsGameTest {
   @Test
   fun `placing two ships (a MOTHERSHIP and DESTROYER) vertically and horizontally fails, when part of them is adjacent`() {
     with(game.addPlayer("John")) {
-      place(MOTHERSHIP, start = Coordinates(A, 2), end = Coordinates(D, 2))
+      place(MOTHERSHIP, from(A, 2), to(D, 2))
 
       val test: () -> Unit = {
-        place(DESTROYER, start = Coordinates(E, 2), end = Coordinates(E, 4))
+        place(DESTROYER, from(E, 2), to(E, 4))
       }
 
       assertThrows<IllegalStateException>(test).let {
@@ -277,9 +276,9 @@ class BattleshipsGameTest {
   @Test
   fun `placing two ships horizontally, with one adjacent row in between, succeeds`() {
     with(game.addPlayer("John")) {
-      place(DESTROYER, start = Coordinates(C, 2), end = Coordinates(C, 4))
+      place(DESTROYER, from(C, 2), to(C, 4))
 
-      val test: () -> Unit = { place(DESTROYER, start = Coordinates(E, 2), end = Coordinates(E, 4)) }
+      val test: () -> Unit = { place(DESTROYER, from(E, 2), to(E, 4)) }
 
       assertDoesNotThrow(test)
     }
@@ -288,9 +287,9 @@ class BattleshipsGameTest {
   @Test
   fun `placing two ships, DESTROYER vertically and MOTHERSHIP horizontally, with space between them, succeeds`() {
     with(game.addPlayer("John")) {
-      place(DESTROYER, start = Coordinates(D, 8), end = Coordinates(F, 8))
+      place(DESTROYER, from(D, 8), to(F, 8))
 
-      val test: () -> Unit = { place(MOTHERSHIP, start = Coordinates(H, 7), end = Coordinates(H, 10)) }
+      val test: () -> Unit = { place(MOTHERSHIP, from(H, 7), to(H, 10)) }
 
       assertDoesNotThrow(test)
     }
@@ -299,9 +298,9 @@ class BattleshipsGameTest {
   @Test
   fun `placing two DESTROYERS, so that they touch in corners of their surrounding area, succeeds`() {
     with(game.addPlayer("John")) {
-      place(DESTROYER, start = Coordinates(E, 5), end = Coordinates(E, 7))
+      place(DESTROYER, from(E, 5), to(E, 7))
 
-      val test: () -> Unit = { place(DESTROYER, start = Coordinates(D, 2), end = Coordinates(D, 4)) }
+      val test: () -> Unit = { place(DESTROYER, from(D, 2), to(D, 4)) }
 
       assertDoesNotThrow(test)
     }
@@ -323,7 +322,7 @@ class BattleshipsGameTest {
   @Test
   fun `placing one MOTHERSHIP is successful`() {
     with(game.addPlayer("John")) {
-      val test: () -> Unit = { place(MOTHERSHIP, start = Coordinates(D, EIGHT), end = Coordinates(G, EIGHT)) }
+      val test: () -> Unit = { place(MOTHERSHIP, from(D, 8), to(G, 8)) }
 
       assertDoesNotThrow(test)
     }
@@ -332,10 +331,10 @@ class BattleshipsGameTest {
   @Test
   fun `placing more than one MOTHERSHIP fails (only 1 is allowed)`() {
     with(game.addPlayer("John")) {
-      place(MOTHERSHIP, start = Coordinates(D, 8), end = Coordinates(G, 8))
-      place(DESTROYER, start = Coordinates(A,3), end = Coordinates(A,5))
+      place(MOTHERSHIP, from(D, 8), to(G, 8))
+      place(DESTROYER, from(A,3), to(A,5))
 
-      val test: () -> Unit = { place(MOTHERSHIP, start = Coordinates(C, 5), end = Coordinates(F, 5)) }
+      val test: () -> Unit = { place(MOTHERSHIP, from(C, 5), to(F, 5)) }
 
       assertThrows<IllegalStateException>(test).let {
         assertThat(it.message).isEqualTo("Only 1 MOTHERSHIP can be placed")
@@ -346,10 +345,10 @@ class BattleshipsGameTest {
   @Test
   fun `placing more than two DESTROYERS fails (only 2 is allowed)`() {
     with(game.addPlayer("John")) {
-      place(DESTROYER, start = Coordinates(D, 8), end = Coordinates(F, 8))
-      place(DESTROYER, start = Coordinates(A,3), end = Coordinates(A,5))
+      place(DESTROYER, from(D, 8), to(F, 8))
+      place(DESTROYER, from(A,3), to(A,5))
 
-      val test: () -> Unit = { place(DESTROYER, start = Coordinates(C, 5), end = Coordinates(E, 5)) }
+      val test: () -> Unit = { place(DESTROYER, from(C, 5), to(E, 5)) }
 
       assertThrows<IllegalStateException>(test).let {
         assertThat(it.message).isEqualTo("Only 2 DESTROYER-s can be placed")
@@ -360,11 +359,11 @@ class BattleshipsGameTest {
   @Test
   fun `placing more than three WARSHIPS fails (only 3 is allowed)`() {
     with(game.addPlayer("John")) {
-      place(WARSHIP, start = Coordinates(D, 8), end = Coordinates(E, 8))
-      place(WARSHIP, start = Coordinates(A,3), end = Coordinates(A,4))
-      place(WARSHIP, start = Coordinates(C, 5), end = Coordinates(D, 5))
+      place(WARSHIP, from(D, 8), to(E, 8))
+      place(WARSHIP, from(A,3), to(A,4))
+      place(WARSHIP, from(C, 5), to(D, 5))
 
-      val test: () -> Unit = { place(WARSHIP, start = Coordinates(J, 7), end = Coordinates(J, 8)) }
+      val test: () -> Unit = { place(WARSHIP, from(J, 7), to(J, 8)) }
 
       assertThrows<IllegalStateException>(test).let {
         assertThat(it.message).isEqualTo("Only 3 WARSHIP-s can be placed")
@@ -434,5 +433,5 @@ class BattleshipsGameTest {
   private fun at(row: Row, column: Int) = Coordinates(row, column)
 
   private fun Player.placeGunshipAt(row: Row, column: Int) =
-    place(GUNSHIP, start = Coordinates(row, column), end = Coordinates(row, column))
+    place(GUNSHIP, from(row, column), to(row, column))
 }
